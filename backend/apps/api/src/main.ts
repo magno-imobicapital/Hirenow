3c01766d6b80d5419d315bc5724bc89a9f1e038a
magno-imobicapital <magno@imobicapital.com.br>
2026-04-07 00:02:47 -0300

    feat: Adicionar cors para todas as origens

--- backend/apps/api/src/main.ts
+++ backend/apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const config = app.get(ConfigService);
   await app.listen(config.getOrThrow<number>('API_PORT'));
